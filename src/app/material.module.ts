a47abda5cd3bf19a1b2bb49a5aa7e31b184b3c76
quickjosh <quickjosh@github.com>
2020-09-24 10:56:37 -0600

    adding icon availability

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,15 @@
+// This page is to add whatever componenent you like to avoid a big fat APP with all material objects dumped in JQ
+
+// This is here for 2 way binding: Two-way binding gives your app a way to share data between a component class and its template.
 import { NgModule } from '@angular/core';
+
+// An example of `hey angular, I just want the button api`
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-    imports:[MatButtonModule],
-    exports:[MatButtonModule]
+    imports:[MatButtonModule, MatIconModule],
+    exports:[MatButtonModule, MatIconModule]
 })
 export class MaterialModule {
 
