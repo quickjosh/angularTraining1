8c2204018c15e87b45d110dded870e1807ed184e
quickjosh <quickjosh@github.com>
2020-09-24 12:04:11 -0600

    adding some new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-
-const routes: Routes = [];
-
+// This is a type
+// This is an array of routes
+const routes: Routes = [
+  { path: '', component: WelcomeComponent},
+{ path: 'signup', component: SignupComponent},
+];
+// This is a decorator
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
